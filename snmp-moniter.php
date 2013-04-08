640d168cf678bf3e20d21aa7042e616579815f8c
Twwy <zje2008@qq.com>
2013-04-08 18:30:05 +0800

    snmp函数更新

--- snmp-moniter.php
+++ snmp-moniter.php
@@ -1,4 +1,51 @@
 <?php
 
+class snmp_moniter{
+	public $ip = false;
+	public $community = 'public';	
+
+	//系统描述
+	public function sysDescr(){
+		return $this->format(snmprealwalk($this->ip, $this->community, 'system.sysDescr.0'));
+	}
+
+	//连续开机时间
+	public function sysUpTime(){
+		return $this->format(snmprealwalk($this->ip, $this->community, 'system.sysUpTime.0'));
+
+	}
+
+	//系统名称
+	public function sysName(){
+                return $this->format(snmprealwalk($this->ip, $this->community, 'system.sysName.0'));
+	}
+
+	//当前连接
+	public function netstat(){
+		$result = snmprealwalk($this->ip, $this->community, '1.3.6.1.2.1.6.13.1.1');
+		$return = array();
+		foreach($result as $key => $value){
+			$name = explode('.', $key);
+			$return[] = array(
+				$name[0],
+				"{$name[1]}.{$name[2]}.{$name[3]}.{$name[4]}",
+				$name[5],
+				"{$name[6]}.{$name[7]}.{$name[8]}.{$name[9]}",
+				$name[10],
+				str_replace('INTEGER: ', '', $value)
+			);
+		}
+		return $return;
+	}
+
+
+	private function format($result){
+		if(!$result) return false;
+		$result = array_shift($result);
+		$result = str_replace('STRING: ','', $result);
+		return $result;
+	}
+
+}
 
 ?>
---
+++ test.php
@@ -0,0 +1,28 @@
+<?php
+
+//61.175.163.196 windows
+//61.175.132.172 windows
+//110.76.43.117 linux
+$ip = '110.76.43.117';
+$ip2 = '61.175.132.172';
+//$ip = '61.175.163.196';
+
+$info = '1.3.6.1.4.1.2021.4';
+
+$a = snmprealwalk($ip, "public", $info); 
+//$a = snmpget($ip, 'public', '1.3.6.1.2.1.1.1.0');
+$b = snmprealwalk($ip2, "public", $info);
+
+
+/*
+require('snmp-moniter.php');
+$snmp = new snmp_moniter;
+$snmp->ip = $ip;
+$a = $b = $snmp->netStat();
+*/
+
+var_dump($a);
+echo '<br/><br/>';
+var_dump($b);
+
+?>
